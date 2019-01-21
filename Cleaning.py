47f1050ba11b623fe3106d34c6a56091f59600e8
Martin <lebourdais.martin@gmail.com>
2019-01-21 14:47:22 +0100

    feature:Cleaning and tokenizing of the article

---
+++ Cleaning.py
@@ -0,0 +1,35 @@
+import nltk
+from nltk.stem import *
+import re
+
+def tokenize(text):
+    list_token=nltk.word_tokenize(text)
+    return list_token
+
+def get_Article(link):
+    fArticle = open(link,'r')
+    rawText = fArticle.read()
+    return rawText
+def stem(tokens):
+    list_stem = []
+    stemmer = SnowballStemmer("english")
+    for token in tokens:
+        list_stem.append(stemmer.stem(token))
+    return list_stem
+def particle_removal(tokens):
+    cleaned_list = []
+    regex = re.compile('[-=@_!#$%^&*()<>?,"/\|}{~:]')
+    for token in tokens:
+        if (regex.search(token) == None):
+            if (len(token)>2):
+                cleaned_list.append(token)
+    return cleaned_list
+def clean(link):
+    text = get_Article(link)
+    tokens=tokenize(text)
+    stemmed_token = stem(tokens)
+    big_words = particle_removal(stemmed_token)
+
+    print(big_words)
+
+clean("0.news")
